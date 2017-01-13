541d54212dc3939702249381a2dfdd1d44058ed2
localghost <zkostrzewa@gmail.com>
2017-01-13 16:43:05 +0100

    Handle missing or invalid template properly.

--- main.go
+++ main.go
@@ -8,10 +8,17 @@ import (
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "Usage: ppp <template-path>")
+		os.Exit(1)
+	}
+
 	templatePath := os.Args[1]
 	result, err := parser.ParseFile(templatePath)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %s\n", err.Error())
+		os.Exit(1)
 	}
+
 	json.NewEncoder(os.Stdout).Encode(result)
 }
